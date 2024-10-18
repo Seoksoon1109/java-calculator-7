bb6c65032aceb4b0e889b6541379ee7f59912bd3
Seokosoon1109 <hyun_21@naver.com>
2024-10-19 01:23:21 +0900

    feat: 기본 구분자(',',':')로 문자열 덧셈 기능 구현

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -1,4 +1,17 @@
 package calculator;
 
 public class CalculatorController {
+    public int add(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열이 입력되었습니다.");
+        }
+
+        String delimiter = ",|:";
+        String[] numbers = input.split(delimiter);
+        return sumNumbers(numbers);
+    }
+
+    private int sumNumbers(String[] numbers) {
+        return 0;
+    }
 }
